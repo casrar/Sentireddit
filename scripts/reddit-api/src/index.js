16d519626a503ce8f0463aa2336bc81e2e886c7b
casrar <casey.j.morar@gmail.com>
2023-03-12 14:52:49 -0400

    Adding scripts folder and cloudflare worker

---
+++ scripts/reddit-api/src/index.js
@@ -0,0 +1,8 @@
+export default {
+  async fetch(request, env) {
+    const params = await new URL(request.url).searchParams
+    const response = await fetch(`https://www.reddit.com/r/${params.get('subreddit')}.json?after=${params.get('after')}&limit=100`)
+    const responseJSON = await response.json()
+    return new Response(responseJSON)
+  }
+}
