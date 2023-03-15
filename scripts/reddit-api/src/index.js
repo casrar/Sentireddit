bfca5ac3c22b311d03116dd74b617efb5e63d6c2
casrar <casey.j.morar@gmail.com>
2023-03-15 13:46:38 -0400

    returning stringified json

--- scripts/reddit-api/src/index.js
+++ scripts/reddit-api/src/index.js
@@ -3,6 +3,6 @@ export default {
     const params = await new URL(request.url).searchParams
     const response = await fetch(`https://www.reddit.com/r/${params.get('subreddit')}.json?after=${params.get('after')}&limit=100`)
     const responseJSON = await response.json()
-    return new Response(responseJSON)
+    return new Response(JSON.stringify(responseJSON))
   }
 }
