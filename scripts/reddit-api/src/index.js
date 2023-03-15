ea3a2dc9c8c9668654444591ca0e2ba49a5fe9be
casrar <casey.j.morar@gmail.com>
2023-03-15 17:03:42 -0400

    rewriting worker to use pushshift, and return empty response if call is not valid

--- scripts/reddit-api/src/index.js
+++ scripts/reddit-api/src/index.js
@@ -1,7 +1,16 @@
 export default {
   async fetch(request, env) {
     const params = await new URL(request.url).searchParams
-    const response = await fetch(`https://www.reddit.com/r/${params.get('subreddit')}.json?after=${params.get('after')}&limit=100`)
+    if (params.get("q") == null) {
+      return new Response()
+    }
+    if (params.get("subreddit") == null) {
+      return new Response()
+    }
+    if (params.get("after") == null) {
+      return new Response()
+    }
+    const response = await fetch(`https://api.pushshift.io/reddit/search/comment/?q=${params.get("q")}&subreddit=${params.get("subreddit")}&after=${params.get("after")}&size=500`)
     const responseJSON = await response.json()
     return new Response(JSON.stringify(responseJSON))
   }
