7221e72a7d0dcee1010c4d526ddb8ef3626fad4a
casrar <casey.j.morar@gmail.com>
2023-05-17 01:24:31 -0400

    Removing unused data from scraping

--- scripts/reddit-api/src/index.js
+++ scripts/reddit-api/src/index.js
@@ -12,6 +12,16 @@ export default {
     }
     const response = await fetch(`https://api.pushshift.io/reddit/search/comment/?q=${params.get("q")}&subreddit=${params.get("subreddit")}&after=${params.get("after")}&size=500`)
     const responseJSON = await response.json()
-    return new Response(JSON.stringify(responseJSON))
+    if (responseJSON["data"] == null) {
+      return new Response()
+    }
+
+    const responseData = { "posts": [] }
+
+    responseJSON["data"].forEach(element => {
+      responseData["posts"].push(element["body"])
+    });
+
+    return new Response(JSON.stringify(responseData))
   }
 }
