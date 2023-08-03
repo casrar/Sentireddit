797fd240e5a3bb9d38a8482cdeb99469b1b7765b
casrar <casey.j.morar@gmail.com>
2023-08-03 15:26:00 -0400

    adding selection box for data_sources

--- src/front-end/frontend.py
+++ src/front-end/frontend.py
@@ -12,14 +12,25 @@ auth_token = response['token']
 
 st.write("Reddit Sentiment Analysis")
 
-data_sources = st.form('data_sources')
-subreddit = data_sources.text_input("Subreddit:")
-search_term = data_sources.text_input("Search Term:")
-data_sources_submit = data_sources.form_submit_button("Add Data Source")
-if data_sources_submit:
+data_source = st.form('data_sources')
+subreddit = data_source.text_input("Subreddit:")
+search_term = data_source.text_input("Search Term:")
+data_source_submit = data_source.form_submit_button("Add Data Source")
+if data_source_submit:
     data = {
             'subreddit': subreddit, 
             'search_term': search_term,
             }
     response = requests.post('http://127.0.0.1:8090/api/collections/data_source/records', json=data, headers={'Authorization': auth_token}).json()
 
+response = requests.get(
+    'http://127.0.0.1:8090/api/collections/data_source/records', 
+    headers={'Authorization': auth_token}).json()
+
+def format_selections(data_source):
+    return f'{data_source["subreddit"]}:{data_source["search_term"]}'
+st.selectbox(label='Choose Data Source:', options=response['items'], format_func=format_selections)
+
+
+# st.metric(label='Change', value='.954', delta='.034')
+# st.line_chart([1, 2, 3])
