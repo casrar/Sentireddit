9876cc12ea41ef43fb3cb92ed22fbd4c7d2271b2
casrar <casey.j.morar@gmail.com>
2023-08-16 19:45:31 -0400

    adding packages and getting db auth token

--- src/sentireddit.py
+++ src/sentireddit.py
@@ -1,4 +1,18 @@
 from flask import Flask
+import requests 
+from dotenv import dotenv_values
+import datetime
+from datetime import date, datetime as dt
+import urllib.parse
+import plotly.express as px
+import pandas as pd
+
+config = dotenv_values(".env")
+# error check and log
+response = requests.post(
+    'http://127.0.0.1:8090/api/collections/users/auth-with-password', 
+    data={'identity': config['IDENTITY'], 'password': config['PASSWORD']}).json()
+auth_token = response['token']
 
 app = Flask(__name__)
 
