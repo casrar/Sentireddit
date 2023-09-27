9c48a583d957ec8487f74f14e5ebf8adca96b6b0
casrar <casey.j.morar@gmail.com>
2023-09-27 17:21:56 -0400

    Working on utils and analytics endpoint

--- src/sentireddit.py
+++ src/sentireddit.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request
 import requests 
 from dotenv import dotenv_values
-import datetime
-from datetime import date, datetime as dt
 import urllib.parse
 import plotly.express as px
 import pandas as pd
@@ -37,7 +35,6 @@ def about():
 @app.route('/data_management', methods=['POST', 'GET'])
 def data_management():
     context = {}
-    print(utils.get_all_data_sources(auth_token)['items'])
     context['data_sources'] = utils.get_all_data_sources(auth_token)['items']
     context['data'] = utils.get_all_data(auth_token)['items']
 
@@ -64,7 +61,6 @@ def remove_data_source():
     context['data_sources'] = utils.get_all_data_sources(auth_token)['items']
 
     return render_template('/partials/data_sources.html', context=context)
-    
 
 @app.route('/remove_data', methods=['DELETE'])
 def remove_data():
@@ -82,56 +78,97 @@ def analytics():
     response = requests.get('http://127.0.0.1:8090/api/collections/data_source/records').json() 
     data = {'x': 'compound list', 'y': [], 'type': 'violin', 'mode': 'markers'}
     layout = {'title': 'Compound'}
-    chart_data = {'data': [data], 'layout': layout}
+    # chart_data = {'data': [data], 'layout': layout}
+    chart_data = None
     context['data_sources'] = response['items']
-    if request.method == 'POST' and request.form['form-id'] == 'select-data-source':
-        # need to grab all data from response, I believe I am not grabbing it all
-        
-        first_date = int (dt.strptime(request.form['first-date'], '%Y-%m-%d').timestamp())
-        second_date = int (dt.strptime(request.form['second-date'], '%Y-%m-%d').timestamp())
-        params = {
-            'filter': f'(data_source=\'{request.form["data-source-selection"]}\' && post_date >= {first_date} && post_date <= {second_date})'
-        }
-        response = requests.get('http://127.0.0.1:8090/api/collections/data/records',
-                                params=params,
-                                headers={'Authorization': auth_token}).json() 
-        items = response['items']
 
-        context['avg_compound'], context['avg_pos'], context['avg_neu'], context['avg_neg'] = utils.calculate_average_sentiments(items)
-        context['avg_compound'] = round(context['avg_compound'], 2)
-        context['avg_pos'] = round(context['avg_pos'], 2)
-        context['avg_neu'] = round(context['avg_neu'], 2) 
-        context['avg_neg'] = round(context['avg_neg'], 2) 
-        
-        context['summary'] = utils.sentiment_observation(context['avg_compound'])
-        compound_list = []
-        for item in items:
-            compound_list.append(item['compound'])
-        print(compound_list)
-        data = {'x': 'compound list', 'y': compound_list, 'type': 'violin', 'mode': 'markers', 'points': 'all'}
-        layout = {'title': 'Compound'}
-        chart_data = {'data': [data], 'layout': layout}
 
-        params = {
-            'sort': '-pos',
-            'perPage': '1',
-            'filter': f'(data_source=\'{request.form["data-source-selection"]}\' && post_date >= {first_date} && post_date <= {second_date})'
-        }
-        response = requests.get('http://127.0.0.1:8090/api/collections/data/records',
-                                params=params,
-                                headers={'Authorization': auth_token}).json() 
-        items = response['items']
-        context['most_positive_post'] = items[0]['body'] if items else 'N/A'
-        params = {
-            'sort': '+neg',
-            'perPage': '1',
-            'filter': f'(data_source=\'{request.form["data-source-selection"]}\' && post_date >= {first_date} && post_date <= {second_date})'
-        }
-        response = requests.get('http://127.0.0.1:8090/api/collections/data/records',
-                                params=params,
-                                headers={'Authorization': auth_token}).json() 
-        items = response['items']
-        context['most_negative_post'] = items[0]['body'] if items else 'N/A'
+    # if request.method == 'POST' and request.form['form-id'] == 'select-data-source':
+    #     # need to grab all data from response, I believe I am not grabbing it all
+        
+    #     first_date = int (dt.strptime(request.form['first-date'], '%Y-%m-%d').timestamp())
+    #     second_date = int (dt.strptime(request.form['second-date'], '%Y-%m-%d').timestamp())
+    #     params = {
+    #         'filter': f'(data_source=\'{request.form["data-source-selection"]}\' && post_date >= {first_date} && post_date <= {second_date})'
+    #     }
+    #     response = requests.get('http://127.0.0.1:8090/api/collections/data/records',
+    #                             params=params,
+    #                             headers={'Authorization': auth_token}).json() 
+    #     items = response['items']
+
+    #     context['avg_compound'], context['avg_pos'], context['avg_neu'], context['avg_neg'] = utils.calculate_average_sentiments(items)
+    #     context['avg_compound'] = round(context['avg_compound'], 2)
+    #     context['avg_pos'] = round(context['avg_pos'], 2)
+    #     context['avg_neu'] = round(context['avg_neu'], 2) 
+    #     context['avg_neg'] = round(context['avg_neg'], 2) 
+        
+    #     context['summary'] = utils.sentiment_observation(context['avg_compound'])
+    #     compound_list = []
+    #     for item in items:
+    #         compound_list.append(item['compound'])
+    #     print(compound_list)
+    #     data = {'x': 'compound list', 'y': compound_list, 'type': 'violin', 'mode': 'markers', 'points': 'all'}
+    #     layout = {'title': 'Compound'}
+    #     chart_data = {'data': [data], 'layout': layout}
+
+    #     params = {
+    #         'sort': '-pos',
+    #         'perPage': '1',
+    #         'filter': f'(data_source=\'{request.form["data-source-selection"]}\' && post_date >= {first_date} && post_date <= {second_date})'
+    #     }
+    #     response = requests.get('http://127.0.0.1:8090/api/collections/data/records',
+    #                             params=params,
+    #                             headers={'Authorization': auth_token}).json() 
+    #     items = response['items']
+    #     context['most_positive_post'] = items[0]['body'] if items else 'N/A'
+    #     params = {
+    #         'sort': '+neg',
+    #         'perPage': '1',
+    #         'filter': f'(data_source=\'{request.form["data-source-selection"]}\' && post_date >= {first_date} && post_date <= {second_date})'
+    #     }
+    #     response = requests.get('http://127.0.0.1:8090/api/collections/data/records',
+    #                             params=params,
+    #                             headers={'Authorization': auth_token}).json() 
+    #     items = response['items']
+    #     context['most_negative_post'] = items[0]['body'] if items else 'N/A'
         
     return render_template('analytics.html', context=context, chart_data=chart_data)
 
+
+@app.route('/update_graph', methods=['POST'])
+def update_graph():
+    context = {}
+    chart_data = None
+    chart_type = request.form['chartType']
+    data_source = request.form['data-source-selection']
+    first_date = request.form['first-date']
+    second_date = request.form['second-date']
+
+    # if data sources not selected, then return nothing
+    if (utils.is_empty(chart_type) or utils.is_empty(data_source) or 
+        utils.is_empty(first_date) or utils.is_empty(second_date)):    
+        return render_template('analytics.html', context=context, chart_data=chart_data)
+    
+    if not (chart_type == 'Compound' or chart_type == 'Positive' or chart_type == 'Neutral' or chart_type == 'Negative'):
+        return render_template('analytics.html', context=context, chart_data=chart_data)
+    
+    items = utils.get_all_data_in_date_range(first_date=first_date, second_date=second_date, data_source=data_source, auth_token=auth_token)['items']
+    x_label = f'{chart_type} list'
+    data = {'x': x_label, 'y': items, 'type': 'violin', 'mode': 'markers'}
+    layout = {'title': chart_type}
+    chart_data = {'data': [data], 'layout': layout}
+    return render_template('analytics.html', context=context, chart_data=chart_data)
+
+@app.route('/update_data_source', methods=['POST'])
+def update_data_source():
+    context = {}
+    response = requests.get('http://127.0.0.1:8090/api/collections/data_source/records').json() 
+    data = {'x': 'compound list', 'y': [], 'type': 'violin', 'mode': 'markers'}
+    layout = {'title': 'Compound'}
+    # chart_data = {'data': [data], 'layout': layout}
+    chart_data = None
+    context['data_sources'] = response['items']
+    # if data sources arent selected then dont return anything
+    # return updated data
+    print(request.form)
+    return render_template('analytics.html', context=context, chart_data=chart_data)
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,14 @@
 from statistics import mean 
 import requests 
+import datetime
+from datetime import date, datetime as dt
 
+def is_empty(x):
+    if x == '':
+        return True
+    if x is None:
+        return True
+    return False
 
 def calculate_average_sentiments(items):
     avg_compound = avg_pos = avg_neu = avg_neg = 0 
@@ -15,47 +23,73 @@ def calculate_average_sentiments(items):
     return mean(compound_list), mean(pos_list), mean(neu_list), mean(neg_list), 
 
 def sentiment_observation(avg_compound): # error with overwhelmingly negative, compound = .3
-    if avg_compound > 0.9:
+    if avg_compound >= 0.9:
         return 'overwhelmingly positive'
-    elif avg_compound < 0.9 and avg_compound > 0.6:
+    elif avg_compound < 0.9 and avg_compound >= 0.6:
         return 'very positive'
-    elif avg_compound < 0.6 and avg_compound > 0.3:
+    elif avg_compound < 0.6 and avg_compound >= 0.3:
         return 'positive'
-    elif avg_compound < 0.3 and avg_compound > 0.15:
+    elif avg_compound < 0.3 and avg_compound >= 0.15:
         return 'slightly positive'
-    elif avg_compound < 0.15 and avg_compound > -0.15:
+    elif avg_compound < 0.15 and avg_compound >= -0.15:
         return 'mixed'
-    elif avg_compound < -0.15 and avg_compound > -0.3:
+    elif avg_compound < -0.15 and avg_compound >= -0.3:
         return 'slightly negative'
-    elif avg_compound < -0.3 and avg_compound > -0.6:
+    elif avg_compound < -0.3 and avg_compound >= -0.6:
         return 'negative'
     elif avg_compound < -0.6 and avg_compound > -0.9:
         return 'very negative'
     else:
         return 'overwhelmingly negative'
 
-def get_all_data(auth_token):
+def ordered_form_date_to_unix(first_date, second_date):
+    first_date = int (dt.strptime(first_date, '%Y-%m-%d').timestamp())
+    second_date = int (dt.strptime(second_date, '%Y-%m-%d').timestamp())
+
+    if second_date < first_date:
+        return (second_date, first_date)
+    return (first_date, second_date)
+
+
+def get_total_records(url, auth_token):
     params = { 'perPage': 1 }
-    response = requests.get('http://127.0.0.1:8090/api/collections/data/records',
+    response = requests.get(url,
                             params=params,
                             headers={'Authorization': auth_token}).json()
     if response['totalItems'] < 1:
         return None
-    params['perPage'] = response['totalItems']
+    return response['totalItems']
+
+def get_all_data(auth_token):
+    per_page = get_total_records('http://127.0.0.1:8090/api/collections/data/records', auth_token=auth_token)
+    if per_page < 1:
+        return None
+    params = { 'perPage': per_page }
     response = requests.get('http://127.0.0.1:8090/api/collections/data/records',
                             params=params,
                             headers={'Authorization': auth_token}).json() 
     return response
 
 def get_all_data_sources(auth_token):
-    params = { 'perPage': 1 }
+    per_page = get_total_records('http://127.0.0.1:8090/api/collections/data_source/records', auth_token=auth_token)
+    if per_page < 1:
+        return None
+    params = { 'perPage': per_page }
     response = requests.get('http://127.0.0.1:8090/api/collections/data_source/records',
                             params=params,
-                            headers={'Authorization': auth_token}).json()
-    if response['totalItems'] < 1:
+                            headers={'Authorization': auth_token}).json() 
+    return response
+
+def get_all_data_in_date_range(first_date, second_date, data_source, auth_token):
+    per_page = get_total_records('http://127.0.0.1:8090/api/collections/data/records', auth_token=auth_token)
+    if per_page < 1:
         return None
-    params['perPage'] = response['totalItems']
-    response = requests.get('http://127.0.0.1:8090/api/collections/data_source/records',
-                                params=params,
-                                headers={'Authorization': auth_token}).json() 
+    dates = ordered_form_date_to_unix(first_date=first_date, second_date=second_date)
+    params = {
+            'per_page': per_page,
+            'filter': f'(data_source=\'{data_source}\' && post_date >= {dates[0]} && post_date <= {dates[1]})'
+        }
+    response = requests.get('http://127.0.0.1:8090/api/collections/data/records',
+                            params=params,
+                            headers={'Authorization': auth_token}).json() 
     return response
