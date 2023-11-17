6ad3136e18cb580556b6e83da20fb692c715ffb4
casrar <casey.j.morar@gmail.com>
2023-11-17 10:59:17 -0500

    changes

--- src/daily-scraper/test.py
+++ src/daily-scraper/test.py
@@ -1,6 +1,9 @@
 import requests 
 import urllib.parse
 import json
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 page_url = 'https://gql.reddit.com/'
 
@@ -8,7 +11,7 @@ data = {
     'id': '6c87f86c5706',
     'variables': {
         'authorsAfter': None,
-        'commentsAfter': 'MjQ=',
+        'commentsAfter': None, # This will change
         'communitiesAfter': None,
         'communitySearch': True,
         'customFeedSearch': False,
@@ -41,7 +44,7 @@ data = {
 }
 
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0',
-           'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNzAwMjU0NjE0LjU5Njc3MiwiaWF0IjoxNzAwMTY4MjE0LjU5Njc3MiwianRpIjoibE9GLTZTOVBubFp6WlZhS1hGX1h4eHhIVlJzNGlnIiwiY2lkIjoiOXRMb0Ywc29wNVJKZ0EiLCJsaWQiOiJ0Ml9xbmViaWt0IiwiYWlkIjoidDJfcW5lYmlrdCIsImxjYSI6MTUxNTAwMTgzNTg1Mywic2NwIjoiZUp4a2tkR090REFJaGQtbDF6N0JfeXBfTmh0c2NZYXNMUWFvazNuN0RWb2NrNzN5d1BuYXd2Rl9ncHk1TjB0VHlvS2xrR21hRWhieXpySUNTWnJTazlSWTNtbEtWTEFabWN1VmRxelE0SUZwU3BWTDVyYlE0OUltTkhkajBiTVRWMVV1S3ctcUFxMm5hbXpqRnJZbnhwbU4xWVpVWEpkVG1jQ3lVRDZMRl8yUXE3ZjItVFA5SnJTRFlVVlZlS0IzQktFY240M0ZGMVBZX1QwMTZkbTZZRkY3cndlcGZhN0g3dHBuelVLemNfczVucjgya25FNUxzMVBzSDkzX1JJNjhNanpNcU1PWU1RYWZwUmhSOWJoWDNVQWxRc0tHRXNRbjBZZ1Z5N2Zfa3V3WFpQLU5XNUhmUEhOaUp2ZThGc3pVTThsaUVPZnh0ZHZBQUFBX18tQ3lkUW4iLCJyY2lkIjoielRBYUluREZPLWdIZVdtYUhXSjZGVmxDaXFST2lNZVJuS1E0OS0tdERIOCIsImZsbyI6Mn0.EfHkSIVR7I2lCsf3zosRMZE23-uvp63xYNuh4uDByyuYo99KAj6-KROVR36fSXWOmDU8gxg_5zbuZWxq1VJXqoXc3ROOBdVITaccWxM2fink4C-IjbucLbr9o522QYj0gXUnvouCb4aPtxF1WVTgz_sChujRcEB9GErB4ZLurAuVMAY7lwQ9mRK3pzvAf2LkyJ8sTHlJgRRkuVsQkuEsRtDw7wo174bE3ks_nZcp703PMLu40HMZLr0uiUMEOZBPl5g77C0S4GhTKvCg_JiAb8oG2np6jIPMZkCjCxAIt271cC2-RwUljrB69EAVclGauXhzWIpMWKStynE6PpNS5g',
+           'Authorization': config['BEARER_TOKEN'],
            'Content-Type': 'application/json'}
 res = requests.post(url=page_url, json=data, headers=headers)
 
